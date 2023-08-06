f8663093d750792af8ad0271b47d7646d5a96610
Xandi <santosal2018@gmail.com>
2023-08-05 22:00:10 -0400

    fix errors in thoughtRoute

--- server/routes/api/thoughtRoute.js
+++ server/routes/api/thoughtRoute.js
@@ -94,33 +94,31 @@ const thoughtController = {
           { $addToSet: { reactions: req.body } },
           { runValidators: true, new: true }
         );
-        
+
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
-      
+
         res.json(dbThoughtData);
       } catch (err) {
-        console.log(err);
         res.status(500).json(err);
       }
     },
     // remove reaction from a thought
-    async removeReaction(req, res) {
+    async deleteReaction(req, res) {
       try {
         const dbThoughtData = await Thought.findOneAndUpdate(
           { _id: req.params.thoughtId },
           { $pull: { reactions: { reactionId: req.params.reactionId } } },
           { runValidators: true, new: true }
         );
-        
+
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
-      
+
         res.json(dbThoughtData);
       } catch (err) {
-        console.log(err);
         res.status(500).json(err);
       }
     },
