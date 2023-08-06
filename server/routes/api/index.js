11a2e0b6328c2e13b2afee7d537865fc5ae57d8a
Xandi <santosal2018@gmail.com>
2023-08-05 23:22:09 -0400

    fix errors on index

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./userRoute');
+const thoughtRoutes = require('./thoughtRoute');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
