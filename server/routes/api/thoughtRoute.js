1b6f122bc9b64fcf240f5febb4abfc4e9be53660
Xandi <santosal2018@gmail.com>
2023-08-05 20:54:00 -0400

    create thought logic

--- server/routes/api/thoughtRoute.js
+++ server/routes/api/thoughtRoute.js
@@ -12,6 +12,40 @@ const thoughtController = {
         res.status(500).json(err);
       }
     },
+     // get thought by id
+    async getThought(req, res) {
+      try {
+        const thoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+
+        if (!thoughtData) {
+          return res.status(404).json({ message: 'No thought was found' });
+        }
+
+        res.json(thoughtData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+    // create a thought
+    async createThought(req, res) {
+      try {
+        const thoughtData = await Thought.create(req.body);
+
+        const userData = await User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: thoughtData._id } },
+          { new: true }
+        );
+
+        if (!userData) {
+          return res.status(404).json({ message: 'Thought created but no user with this id!' });
+        }
+
+        res.json({ message: 'Thought has been created' });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
 
   };
   
