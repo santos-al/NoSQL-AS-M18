b1ba7c2b5881c8cd76bf6ff80de4fbb2bbe737fa
Xandi <santosal2018@gmail.com>
2023-08-05 21:52:51 -0400

    add reaction logic into thoughtRoute

--- server/routes/api/thoughtRoute.js
+++ server/routes/api/thoughtRoute.js
@@ -86,6 +86,44 @@ const thoughtController = {
       res.status(500).json(err);
     }
     },
+    // add a reaction to a thought
+    async addReaction(req, res) {
+      try {
+        const dbThoughtData = await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $addToSet: { reactions: req.body } },
+          { runValidators: true, new: true }
+        );
+        
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+      
+        res.json(dbThoughtData);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    },
+    // remove reaction from a thought
+    async removeReaction(req, res) {
+      try {
+        const dbThoughtData = await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $pull: { reactions: { reactionId: req.params.reactionId } } },
+          { runValidators: true, new: true }
+        );
+        
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+      
+        res.json(dbThoughtData);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    },
 
   };
   
